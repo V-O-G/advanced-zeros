476ff04b9a3fcbf2edf75ade0718859ef2698a4e
V-O-G <cornflower1989@gmail.com>
2018-02-23 18:13:39 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var factors = [],
+               prime,
+               power=1;
+           function primeFactors(n){
+               var divisor = 2;
+               while(n>=2){
+                   if(n % divisor === 0){
+                       factors.push(divisor);
+                       n= n/divisor;
+                   }
+                   else{
+                       if(divisor%2===1){divisor+=2; continue}
+                       divisor++;
+                   }
+               }
+               return factors;
+           }
+           primeFactors(base);
+           factors.sort(function(a,b){return b-a});
+           prime = factors[0];
+           function findPower(m){
+               i=1;
+               while(factors[i]===m){
+                   power++;
+                   i++;
+               }
+           }
+           if(factors.length>1){
+               findPower(prime);
+           }
+           var summands = [];
+           i = 1;
+           while (true){
+               var devisor = Math.pow(prime,i);
+               summands.push(Math.floor(number/devisor));
+               i++;
+               if(number/devisor<1){break;}
+           }
+           var answer = summands.reduce(function(a, b) {return a+b;}, 0);
+           answer = Math.floor(answer/power);
+           return answer;
 }
